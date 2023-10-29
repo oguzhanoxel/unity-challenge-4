7dd9bdc8be329deeaa73de375921c68a9a06a25d
oguzhanoxel <oguzhanoksel@hotmail.com>
2023-10-29 20:29:00 +0300

    Watch Where You’re Going

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    private GameObject focalPoint;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRigidbody.AddForce(focalPoint.transform.forward * verticalInput * speed);
+    }
+}
